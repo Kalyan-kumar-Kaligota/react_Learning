809e997193823b4248ec20dc59061fc3f26db1e6
Kalyan-kumar-Kaligota <kalyankannabhai@gmail.com>
2026-07-20 15:41:38 +0530

    initial commit

---
+++ AI-Learning/Day-01/EmployeeDetailsArray.py
@@ -0,0 +1,18 @@
+employee = [
+    {
+        "name" : "kalyan Kumar",
+        "experience" : 3.5,
+        "designation" : "Software Engineer"
+    },
+    {
+        "name" : "Venkat Ramana",
+        "experience" : 3,
+        "designation" : "UI Developer"
+    }
+]
+
+print("Employee Details:")
+for emp in employee:
+    print("Name:", emp["name"], "Experience:", emp["experience"], "years", "Designation:", emp["designation"])
+
+    print(f"Name: {emp['name']}")
---
+++ AI-Learning/Day-01/Variables.py
@@ -0,0 +1,5 @@
+name = "kalyan Kumar"
+experience = 3.5
+designation = "Software Engineer"
+
+print("Name:", name ,"Experience:", experience, "years", "Designation:", designation)
